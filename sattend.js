06d4ebb27f43d6bf682688afd65d716b0a310131
wsiterv <rakshitverma251@gmail.com>
2024-07-16 11:13:34 +0530

    Update sattend.js

--- sattend.js
+++ sattend.js
@@ -1,3 +1,11 @@
-function attendance(){
-    window.location.href = '2.html';
+
+function rpage() {
+    var uname = document.getElementById('name');
+    var roll = document.getElementById('roll');
+
+    if (uname.value.trim() === '' || roll.value.trim() === '') {
+        alert('Please enter both your name and roll number.');
+    } else {
+        window.location.replace('2.html');
+    }
 }
